4ab71e19e6930467b8112168f7ab88c254c6dc29
wayne <waynechen@eztravel.com.tw>
2017-06-12 17:51:45 +0800

    node

---
+++ mail.js
@@ -0,0 +1,28 @@
+var mailer = require("nodemailer");
+
+// Use Smtp Protocol to send Email
+var smtpTransport = mailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "mose286778@gmail.com",
+        pass: "m14911491"
+    }
+});
+
+var mail = {
+    from: "Yashwant Chavan <from@gmail.com>",
+    to: "yooozoo@gmail.com",
+    subject: "Send Email Using Node.js",
+    text: "Node.js New world for me",
+    html: "<b>Node.js New world for me</b>"
+}
+
+smtpTransport.sendMail(mail, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    smtpTransport.close();
+});
