a523412ff2a574818b0411c5526d41f5ea96acb1
WAYNE1894\wayne <mose286778@gmail.com>
2017-06-24 19:00:26 +0800

    update

--- request.js
+++ request.js
@@ -1,11 +1,94 @@
+var http = require('http');
 var request = require('request');
 var cheerio = require("cheerio");
+var scrape = require('html-metadata');
 
-request('https://infometro.cc', function (error, response, body) {
-  //console.log('error:', error); // Print the error if one occurred 
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-  //console.log('body:', body); // Print the HTML for the Google homepage. 
-	
-	$ = cheerio.load(body);
-	console.log($("#logo2_parent .logo2").attr("src"));
-});
+var webshot = require('webshot');
+var fs      = require('fs');
+
+
+
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+  try {
+    var url=req.url.split("?url=")[1];
+    
+//    parser2_img(url,function(base64){
+//       res.end("base64");
+//    })
+    
+    parser_html(url,function(url_info){
+      res.end(JSON.stringify(url_info));
+    })
+    
+//    parser_img(url,function(base64){
+//       res.end(base64);
+//    })
+  } catch(err) {
+    res.end("error1");
+  }
+}).listen(3000);
+
+function parser2_img(url,callback){
+  //https://www.npmjs.com/package/pageres
+  const Pageres = require('pageres');
+
+  const pageres = new Pageres()
+	.src('https://infometro.cc', ['1280x1024'])
+	.dest(__dirname)
+	.run()
+	.then(() => callback("base64"));
+}
+
+function parser_img(url,callback){
+  //https://github.com/brenden/node-webshot
+  
+  var options = {
+  screenSize: {
+    width: 1024
+  , height: 768
+  }
+  , shotSize: {
+    width: 1024/2
+  , height: 768/2
+  }
+  };
+  var renderStream = webshot('https://infometro.cc',options);
+
+  renderStream.on('data', function(data) {
+    var base64=data.toString('base64')
+    callback(base64);
+  });
+}
+function parser_html(url,callback){
+  request(url, function (error,response,html) {
+      var url_info = {}
+      scrape(url,function(error, metadata){
+        if(error){
+          callback(error);
+        }else{
+          var url_info = {}
+          if(metadata.openGraph && metadata.openGraph.description){
+            url_info.description=metadata.openGraph.description;
+          }else if(metadata.general){
+            url_info.description=metadata.general.description;
+          }
+
+          if(metadata.openGraph && metadata.openGraph.title){
+            url_info.title=metadata.openGraph.title;
+          }else if(metadata.general){
+            url_info.title=metadata.general.title;
+          }
+          
+          
+          callback(url_info)
+        }
+      })
+  });
+}
+
+
+
+function print(a){
+  console.log(a)
+}
